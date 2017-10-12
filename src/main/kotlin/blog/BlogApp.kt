cc47b6606c56ce2d4e795146590c3ef3887b475f
GOP-LINK\k.lhoest <k.lhoest@groupeonepoint.com>
2017-10-12 12:00:59 +0200

    AI select target

--- src/main/kotlin/blog/BlogApp.kt
+++ src/main/kotlin/blog/BlogApp.kt
@@ -4,7 +4,6 @@ package blog
 import com.github.salomonbrys.kotson.jsonArray
 import com.github.salomonbrys.kotson.jsonObject*/
 import com.google.gson.Gson
-import com.google.gson.JsonObject
 import org.jetbrains.ktor.netty.*
 import org.jetbrains.ktor.routing.*
 import org.jetbrains.ktor.application.*
@@ -18,6 +17,7 @@ import org.jetbrains.ktor.features.CallLogging
 //import org.jetbrains.ktor.heroku.module
 import java.lang.Integer.max
 import java.lang.Integer.min
+import java.util.TreeSet
 
 
 data class ReturnJSON(var fleets: ArrayList<FleetOrder> = ArrayList(), var terraformings: ArrayList<TeraformOrder> = ArrayList())
@@ -51,20 +51,32 @@ fun Application.module() {
                 call.respond(returnJSON)
             }
             if (inputPojo != null) {
-                val solarSystem = inputPojo.planets.filterNotNull()
-                val laRebelion = solarSystem.filter { planet: Planet -> planet.owner!! == PlanetIA.ME; }
-                val lEmpire = solarSystem.filter { planet: Planet -> planet.owner!! != PlanetIA.ME; }
+                val solarSystemPojo = inputPojo.planets.filterNotNull()
+
+                val laRebelionPojo = solarSystemPojo.filter { planet: Planet -> planet.owner!! == WorkablePlanet.ME; }
+                val lEmpirePojo = solarSystemPojo.filter { planet: Planet -> planet.owner!! != WorkablePlanet.ME; }
 
                 val allFleet = inputPojo.fleets!!.filterNotNull()
-                val rebelionFleet = allFleet.filter { fleet -> fleet.owner!! == PlanetIA.ME; }
-                val empireFleet = allFleet.filter { fleet -> fleet.owner!! != PlanetIA.ME; }
-                try {
-                    for (colony in laRebelion) {
-                        var planetIA = PlanetIA(colony, laRebelion, lEmpire, rebelionFleet, empireFleet)
+                val rebelionFleet = allFleet.filter { fleet -> fleet.owner!! == WorkablePlanet.ME; }
+                val empireFleet = allFleet.filter { fleet -> fleet.owner!! != WorkablePlanet.ME; }
+
+                val solarSystem = TreeSet<WorkablePlanet>()
+                for (inspectPlanet in solarSystemPojo) {
+                    if (inspectPlanet.owner == WorkablePlanet.ME) {
+                        solarSystem.add(PlanetRebel(inspectPlanet, laRebelionPojo, lEmpirePojo, rebelionFleet, empireFleet))
+                    } else {
+                        solarSystem.add(WorkablePlanet(inspectPlanet, laRebelionPojo, lEmpirePojo, rebelionFleet, empireFleet))
                     }
-                } catch (e: Exception) {
-                    call.respond(returnJSON)
                 }
+                val it = solarSystem.iterator();
+                val target = findTarget(it);
+                for (inspectPlanet in solarSystem) {
+                    if(inspectPlanet is PlanetRebel) {
+                        inspectPlanet.pourFrodon(target)
+                    }
+                }
+
+                call.respond(returnJSON)
             }
             //var sortedPlanet:List<Planet?>? = IA.sortPlanetByDistance(returnPojo.solarSystem[0]!! ,returnPojo.solarSystem)
             call.respond(returnJSON)
@@ -76,7 +88,7 @@ fun Application.module() {
 
 fun main(args: Array<String>) {
     val port = Integer.valueOf(System.getenv("PORT"))
-    embeddedServer(Netty, port= port, watchPaths = listOf("BlogAppKt"), module = Application::module).start()
+    embeddedServer(Netty, port = port, watchPaths = listOf("BlogAppKt"), module = Application::module).start()
 }
 
 fun generatePojo(inputBody: String): Pojo {
@@ -84,43 +96,46 @@ fun generatePojo(inputBody: String): Pojo {
     return gson.fromJson<Pojo>(inputBody, Pojo::class.java)
 }
 
-class PlanetIA(val colony: Planet, val laRebelion: List<Planet>, val lEmpire: List<Planet>, val rebelionFleet:List<Fleet>, val empireFleet:List<Fleet>) {
-    //var inpect: Planet;
-    val sendableUnits: Int
-        get() = max(colony.units!! - minPop, 0);
-    val remainingPlace: Int
-        get() = colony.mu!! - colony.units!!
-    val threaten: Boolean
-        get() = (colony.units!! + rebelionFleetIncoming -rebelionFleetIncoming) <= 0
-    val minPop: Int
-        get() = enemyCivilainNearby.toInt() + empireFleetIncoming - rebelionFleetIncoming + 1 //warning: can be negative
-    val empireFleetIncoming:Int
-        get() = getIncomingFleet(fleetNationality=empireFleet)
-    val rebelionFleetIncoming:Int
+fun findTarget(it: Iterator<WorkablePlanet>): Int {
+    val target: Int
+    while (it.hasNext()) {
+        if (it.next() is PlanetRebel) {
+            if((it.next() as PlanetRebel).threaten)
+                break
+        } else {
+            break
+        }
+    }
+    target = it.next().colony.id
+    return target
+}
+
+open class WorkablePlanet(val colony: Planet, val laRebelion: List<Planet>, val lEmpire: List<Planet>, val rebelionFleet: List<Fleet>, val empireFleet: List<Fleet>) : Comparable<PlanetRebel> {
+
+    val empireFleetIncoming: Int
+        get() = getIncomingFleet(fleetNationality = empireFleet)
+    val rebelionFleetIncoming: Int
         get() = getIncomingFleet(fleetNationality = rebelionFleet)
-    val enemyCivilainNearby:Double
+    val enemyCivilainNearby: Double
         get() {
             var result = 0.0
-            for(inspectPlanet in lEmpire) {
-                result += inspectPlanet.units!!/distance(colony, inspectPlanet)
+            for (inspectPlanet in lEmpire) {
+                result += inspectPlanet.units!! / distance(colony, inspectPlanet)
             }
             return result
         }
 
-    init {
-        for(deathStar in lEmpire) {
-            returnJSON.fleets.add(FleetOrder(min(3, sendableUnits), source = colony.id, target = deathStar.id))
-        }
-
-    }
-
-    private fun getIncomingFleet(fleetNationality:List<Fleet>): Int {
+    protected fun getIncomingFleet(fleetNationality: List<Fleet>): Int {
         var result = 0
         val incomingFleet = fleetNationality.filter({ fleet -> fleet.to == colony.id })
-        for(inpectFleet in incomingFleet) {
+        for (inpectFleet in incomingFleet) {
             result += inpectFleet.units ?: 0
         }
         return result
+    }
+
+    override fun compareTo(other: PlanetRebel): Int {
+        return (colony.gr!! - other.colony.gr!!) * 100 + colony.id % 100 // we add the add to make sure that each planet have a different comparable value
     }
 
     companion object {
@@ -128,14 +143,45 @@ class PlanetIA(val colony: Planet, val laRebelion: List<Planet>, val lEmpire: Li
         const val Guilde_du_Commerce: Int = 0
         const val ME: Int = 1
 
-        @JvmStatic
+        /*@JvmStatic
         fun sortPlanetByDistance(from: Planet, planets: List<Planet?>): List<Planet?>? {
             var filterplanets = planets.filterNotNull();
             filterplanets = filterplanets.filter { planet -> planet?.owner != 1 }
-            return filterplanets.sortedBy { to: Planet -> PlanetIA.distance(from, to) }
-        }
+            return filterplanets.sortedBy { to: Planet -> PlanetRebel.distance(from, to) }
+        }*/
 
         @JvmStatic
         fun distance(planet1: Planet, planet2: Planet): Double = Math.hypot(planet1.x - planet2.x, planet1.y - planet2.y)
+    }
+}
+
+class PlanetRebel(colony: Planet, laRebelion: List<Planet>, lEmpire: List<Planet>, rebelionFleet: List<Fleet>, empireFleet: List<Fleet>)
+    : WorkablePlanet(colony, laRebelion, lEmpire, rebelionFleet, empireFleet) {
+    //var inpect: Planet;
+    val sendableUnits: Int
+        get() = max(colony.units!! - minPop, 0);
+    val remainingPlace: Int
+        get() = colony.mu!! - colony.units!!
+    val threaten: Boolean
+        get() = (colony.units!! + rebelionFleetIncoming - rebelionFleetIncoming) <= 0
+    val minPop: Int
+        get() = enemyCivilainNearby.toInt() + empireFleetIncoming - rebelionFleetIncoming + 1 //warning: can be negative
+
+
+    init {
+        for (deathStar in lEmpire) {
+
+        }
+
+    }
+
+    override fun compareTo(other: PlanetRebel): Int {
+        return colony.gr!! - other.colony.gr!!
+    }
+
+    fun pourFrodon(target: Int) {
+        if(sendableUnits >= 3) {
+            returnJSON.fleets.add(FleetOrder(sendableUnits, source = colony.id, target = target))
+        }
     }
 }
